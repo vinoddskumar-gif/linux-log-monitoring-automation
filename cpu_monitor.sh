f2bd74c85a05b0155b6a60e472fa84a249505a13
Vinod Kumar <vinod.ds.kumar@gmail.com>
2026-04-02 06:30:42 +0530

    Enhanced CPU monitor script

--- cpu_monitor.sh
+++ cpu_monitor.sh
@@ -12,9 +12,19 @@ echo "CPU used : $CPU %"
 if (( $(echo "$CPU > 90" | bc -l) ))
 then 
 	echo "STATUS : CRITICAL"
+	echo " Top CPU consuming processes"
+	ps -eo pid,user,cmd,%cpu --sort=-%cpu | head -5
+	echo
+	echo
+
 elif (( $(echo "$CPU > 75" | bc -l)))
 then 
 	echo " STATUS : HIGH"
+	echo " Top CPU consuming processes"
+        ps -eo pid,user,cmd,%cpu --sort=-%cpu | head -5
+	echo
+	echo
+
 elif (( $(echo "$CPU > 60" | bc -l)))
 then
 	echo " STATUS : WARNING "
